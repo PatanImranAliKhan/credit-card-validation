d728f7ce37832c61adddbaf7a3f030bd9ecb7b34
Patan Khan <patan_khan@bonify.de>
2023-09-25 23:16:40 +0530

    Changes to display card content only after card image was loaded completely

--- src/components/CardBack.jsx
+++ src/components/CardBack.jsx
@@ -1,20 +1,29 @@
+import { useState } from 'react'
 import './styles/card.css'
 
 const CardBack = ({ cvc, cvcError, submitClicked }) => {
-  
+
+  const [loaded, setloaded] = useState(false)
+
   return (
     <div className='card'>
-      <img src="/assets/backcard.jpg" alt="Card Back" />
-      <div className='data'>
-        <span className='cvcdata'>
-          {
-            cvcError === "" && cvc !== "" && submitClicked ?
-              <span>{cvc}</span>
-              :
-              <span>000</span>
-          }
-        </span>
-      </div>
+      <img src="/assets/backcard.jpg" alt="Card Back" onLoad={()=>{setloaded(true)}} />
+      {
+        loaded && (
+          <div>
+            <div className='data'>
+              <span className='cvcdata'>
+                {
+                  cvcError === "" && cvc !== "" && submitClicked ?
+                    <span>{cvc}</span>
+                    :
+                    <span>000</span>
+                }
+              </span>
+            </div>
+          </div>
+        )
+      }
     </div>
   )
 }
--- src/components/CardFront.jsx
+++ src/components/CardFront.jsx
@@ -1,57 +1,66 @@
+import { useState } from 'react'
 import './styles/card.css'
 
 const CardFront = ({ cardDetails, cardDetailsError, submitClicked }) => {
 
+  const [loaded, setloaded] = useState(false)
+
   return (
     <div className='card'>
-      <img src="/assets/frontcard.png" alt="Card Front" />
-      <div className='topcircles'>
-        <span className='leftcircle'></span>
-        <span className='rightcircle'></span>
-      </div>
-      <div className='card_content'>
-        <div className='card_num'>
-          {
-            cardDetailsError.cardNumber !== "" || cardDetails.cardNumber === "" || !submitClicked ?
-              <span>0000  0000  0000  0000</span>
-              :
-              <span>
-                {cardDetails.cardNumber.toString().replace(/\d{4}(?=.)/g, '$& ')}
-              </span>
-          }
-        </div>
-        <div className='card_det'>
+      <img src="/assets/frontcard.png" alt="Card Front" onLoad={() => { setloaded(true) }} />
+      {
+        loaded && (
           <div>
-            {
-              cardDetailsError.cardHolderName !== '' || cardDetails.cardHolderName === "" || !submitClicked ?
-                <span>Mr. XYZ</span>
-                :
-                <span>
-                  {cardDetails.cardHolderName}
-                </span>
-            }
+            <div className='topcircles'>
+              <span className='leftcircle'></span>
+              <span className='rightcircle'></span>
+            </div>
+            <div className='card_content'>
+              <div className='card_num'>
+                {
+                  cardDetailsError.cardNumber !== "" || cardDetails.cardNumber === "" || !submitClicked ?
+                    <span>0000  0000  0000  0000</span>
+                    :
+                    <span>
+                      {cardDetails.cardNumber.toString().replace(/\d{4}(?=.)/g, '$& ')}
+                    </span>
+                }
+              </div>
+              <div className='card_det'>
+                <div>
+                  {
+                    cardDetailsError.cardHolderName !== '' || cardDetails.cardHolderName === "" || !submitClicked ?
+                      <span>Mr. XYZ</span>
+                      :
+                      <span>
+                        {cardDetails.cardHolderName}
+                      </span>
+                  }
+                </div>
+                <div>
+                  {
+                    cardDetailsError.expMonth !== '' || cardDetails.expMonth === "" || !submitClicked ?
+                      <span>MM</span>
+                      :
+                      <span>
+                        {cardDetails.expMonth}
+                      </span>
+                  }
+                  <span>/</span>
+                  {
+                    cardDetailsError.expYear !== '' || cardDetails.expYear === "" || !submitClicked ?
+                      (<span>YY</span>)
+                      :
+                      <span>
+                        {cardDetails.expYear}
+                      </span>
+                  }
+                </div>
+              </div>
+            </div>
           </div>
-          <div>
-            {
-              cardDetailsError.expMonth !== '' || cardDetails.expMonth === "" || !submitClicked ?
-                <span>MM</span>
-                :
-                <span>
-                  {cardDetails.expMonth}
-                </span>
-            }
-            <span>/</span>
-            {
-              cardDetailsError.expYear !== '' || cardDetails.expYear === "" || !submitClicked ?
-                (<span>YY</span>)
-                :
-                <span>
-                  {cardDetails.expYear}
-                </span>
-            }
-          </div>
-        </div>
-      </div>
+        )
+      }
     </div>
   )
 }
